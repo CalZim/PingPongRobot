8509d54885fd96e8bb651411736cb4ca331c412a
Shane Cranor <flamefightthefire@gmail.com>
2016-07-27 11:56:09 -0400

    Removed arbitrary values

--- TurretBotMain.ino
+++ TurretBotMain.ino
@@ -2,10 +2,11 @@
 #define launchMotors 12
 #define feedServoPin 11
 Servo feedServo;
+const int FEEDANGLE = 120;
 void setup() {
   setupMotors();
   powerLaunchMotors();
-  closeFeed(120); //close feed to prevent projectiles from escaping prematurely
+  closeFeed(FEEDANGLE); //close feed to prevent projectiles from escaping prematurely
   delay(10000); //wait for the motors to accelerate before engaging the feed servos
 }
 
