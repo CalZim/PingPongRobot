46f9a6bab5ba32cf5bcd10643460ea8f4c969e54
Shane Cranor <flamefightthefire@gmail.com>
2016-07-28 11:42:02 -0400

    Changed servotype

--- TurretBotMain.ino
+++ TurretBotMain.ino
@@ -1,16 +1,19 @@
 #include <Servo.h>
 #define launchMotors 12
 #define feedServoPin 11
+
 Servo feedServo;
-const int FEEDANGLE = 120;
+
+const int SINGLEMILLIS = 110; //how long will the servo be rotated in milliseconds to drop 1 projectile
+
 void setup() {
   setupMotors();
   powerLaunchMotors();
-  closeFeed(FEEDANGLE); //close feed to prevent projectiles from escaping prematurely
-  delay(10000); //wait for the motors to accelerate before engaging the feed servos
+  closeFeed(); //close feed to prevent projectiles from escaping prematurely
+  //delay(10000); //wait for the motors to accelerate before engaging the feed servos
 }
 
 void loop() {
-  feedSingle();
+  feedAmmo(1);
   delay(1000);
 }
